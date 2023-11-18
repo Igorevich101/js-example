29cb9ad488ef127c9e99e5b5c6ab9accbf00f115
igorevich101 <igorevich101@gmail.com>
2023-11-18 16:30:04 +0200

    add solution variables and operators

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,23 @@
-alert('Hello World')
+let num1 = 2;
+let num2 = 4;
+let num3 = num1+num2;
+console.log(num3);
+let num4 = num3-50;
+console.log(num4);
+let num5 = num4*num3;
+console.log(num5);
+let num6 = num5/2.687;
+console.log(num6);
+let num7 = num5%2.687;
+console.log(num7);
+console.log(num7<0);
+
+let num = 5;
+
+num += 10;
+
+num *= 2;
+
+num -= 4;
+
+num /= 6;
